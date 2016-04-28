8172ede40347c5ddaa29f158273fc2bbd6af1ca4
Skjamal <satinder.jamal@gmail.com>
2016-04-28 00:26:02 +0000

    Push error to resolve

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Flixter::Application.routes.draw do
     root 'static_pages#index'
     resources :courses, only: [:index, :show]
     resources :lessons, only: [:show]
+    
     namespace :instructor do
+      resources :lessons, only: [:update]
+      resources :sections, only: [:update]
       resources :sections, only: [] do
-        resources :lessons, only: [:new, :create]
-      end
-          resources :courses, only: [:new, :create, :show] do
-            resources :sections, only: [:new, :create]
+        resources :lessons, only: [:create]
         end
       resources :courses, only: [:new, :create, :show] do
         resources :sections,  only: [:create]
